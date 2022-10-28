a38dc26007dd0dd5475b62a85cdc2a6b87f10a08
Fan Wu <fan@turintech.ai>
2022-10-28 16:52:52 +0100

    feat: removed recommender model

--- src/hearthstone_card_recommendation/model/__init__.py
+++ src/hearthstone_card_recommendation/model/__init__.py
@@ -2,5 +2,4 @@ from .abstract_model import RecommendModel
 from .naive_graph import NaiveGraph
 from .simple_popularity import SimplePopularity
 from .ensemble import EnsembleKnowledge
-from .recommender import BPRRecommender
 from .similarity import SimilarityModel
